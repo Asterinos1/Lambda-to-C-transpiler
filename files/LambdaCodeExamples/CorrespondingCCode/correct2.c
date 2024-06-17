72e61c928087f47bdf7ca21cfb4c70c284399ffd
Asterinos Karalis <79656046+Asterinos1@users.noreply.github.com>
2024-06-17 17:24:46 +0300

    Create correct2.c

---
+++ files/LambdaCodeExamples/CorrespondingCCode/correct2.c
@@ -0,0 +1,371 @@
+// Token KEYWORD_DEF:			def
+// Token IDENTIFIER:			binarySearch
+// Token LEFT_PARENTHESIS:		(
+// Token IDENTIFIER:			arr
+// Token LEFT_BRACKET:		[
+// Token RIGHT_BRACKET:		]
+// Token COLON:			:
+// Token KEYWORD_INT:			integer
+// Token COMMA:			,
+// Token IDENTIFIER:			size
+// Token COLON:			:
+// Token KEYWORD_INT:			integer
+// Token COMMA:			,
+// Token IDENTIFIER:			x
+// Token COLON:			:
+// Token KEYWORD_INT:			integer
+// Token RIGHT_PARENTHESIS:		)
+// Token MINUS_OP:			-
+// Token GREATER_OP:			>
+// Token KEYWORD_INT:			integer
+// Token COLON:			:
+// Token IDENTIFIER:			l
+// Token COMMA:			,
+// Token IDENTIFIER:			r
+// Token COMMA:			,
+// Token IDENTIFIER:			mid
+// Token COLON:			:
+// Token KEYWORD_INT:			integer
+// Token SEMICOLON:			;
+// Token IDENTIFIER:			l
+// Token ASSIGN_OP:			=
+// Token CONST_INT:			0
+// Token SEMICOLON:			;
+// Token IDENTIFIER:			r
+// Token ASSIGN_OP:			=
+// Token IDENTIFIER:			size
+// Token MINUS_OP:			-
+// Token CONST_INT:			1
+// Token SEMICOLON:			;
+// Token KEYWORD_WHILE:			while
+// Token LEFT_PARENTHESIS:		(
+// Token IDENTIFIER:			l
+// Token LESS_EQUAL_OP:			<=
+// Token IDENTIFIER:			r
+// Token RIGHT_PARENTHESIS:		)
+// Token COLON:			:
+// Token IDENTIFIER:			mid
+// Token ASSIGN_OP:			=
+// Token IDENTIFIER:			l
+// Token PLUS_OP:			+
+// Token LEFT_PARENTHESIS:		(
+// Token IDENTIFIER:			r
+// Token MINUS_OP:			-
+// Token IDENTIFIER:			l
+// Token RIGHT_PARENTHESIS:		)
+// Token DIVIDE_OP:			/
+// Token CONST_INT:			2
+// Token SEMICOLON:			;
+// Token KEYWORD_IF:			if
+// Token LEFT_PARENTHESIS:		(
+// Token IDENTIFIER:			arr
+// Token LEFT_BRACKET:		[
+// Token IDENTIFIER:			mid
+// Token RIGHT_BRACKET:		]
+// Token EQUAL_OP:			==
+// Token IDENTIFIER:			x
+// Token RIGHT_PARENTHESIS:		)
+// Token COLON:			:
+// Token KEYWORD_RETURN:			return
+// Token IDENTIFIER:			mid
+// Token SEMICOLON:			;
+// Token KEYWORD_ENDIF:			endif
+// Token SEMICOLON:			;
+// Token KEYWORD_IF:			if
+// Token LEFT_PARENTHESIS:		(
+// Token IDENTIFIER:			arr
+// Token LEFT_BRACKET:		[
+// Token IDENTIFIER:			mid
+// Token RIGHT_BRACKET:		]
+// Token LESS_OP:			<
+// Token IDENTIFIER:			x
+// Token RIGHT_PARENTHESIS:		)
+// Token COLON:			:
+// Token IDENTIFIER:			l
+// Token ASSIGN_OP:			=
+// Token IDENTIFIER:			mid
+// Token PLUS_OP:			+
+// Token CONST_INT:			1
+// Token SEMICOLON:			;
+// Token KEYWORD_ELSE:			else
+// Token COLON:			:
+// Token IDENTIFIER:			r
+// Token ASSIGN_OP:			=
+// Token IDENTIFIER:			mid
+// Token MINUS_OP:			-
+// Token CONST_INT:			1
+// Token SEMICOLON:			;
+// Token KEYWORD_ENDIF:			endif
+// Token SEMICOLON:			;
+// Token KEYWORD_ENDWHILE:			endwhile
+// Token SEMICOLON:			;
+// Token KEYWORD_RETURN:			return
+// Token MINUS_OP:			-
+// Token CONST_INT:			1
+// Token SEMICOLON:			;
+// Token KEYWORD_ENDDEF:			enddef
+// Token SEMICOLON:			;
+// Token KEYWORD_DEF:			def
+// Token IDENTIFIER:			printArray
+// Token LEFT_PARENTHESIS:		(
+// Token IDENTIFIER:			arr
+// Token LEFT_BRACKET:		[
+// Token RIGHT_BRACKET:		]
+// Token COLON:			:
+// Token KEYWORD_INT:			integer
+// Token COMMA:			,
+// Token IDENTIFIER:			size
+// Token COLON:			:
+// Token KEYWORD_INT:			integer
+// Token RIGHT_PARENTHESIS:		)
+// Token COLON:			:
+// Token KEYWORD_FOR:			for
+// Token IDENTIFIER:			i
+// Token KEYWORD_IN:			in
+// Token LEFT_BRACKET:		[
+// Token CONST_INT:			0
+// Token COLON:			:
+// Token IDENTIFIER:			size
+// Token MINUS_OP:			-
+// Token CONST_INT:			1
+// Token RIGHT_BRACKET:		]
+// Token COLON:			:
+// Token FN_writeInteger:			writeInteger
+// Token LEFT_PARENTHESIS:		(
+// Token IDENTIFIER:			arr
+// Token LEFT_BRACKET:		[
+// Token IDENTIFIER:			i
+// Token RIGHT_BRACKET:		]
+// Token RIGHT_PARENTHESIS:		)
+// Token SEMICOLON:			;
+// Token KEYWORD_IF:			if
+// Token LEFT_PARENTHESIS:		(
+// Token IDENTIFIER:			i
+// Token NOT_EQUAL_OP:			!=
+// Token IDENTIFIER:			size
+// Token MINUS_OP:			-
+// Token CONST_INT:			1
+// Token RIGHT_PARENTHESIS:		)
+// Token COLON:			:
+// Token IDENTIFIER:			writeStr
+// Token LEFT_PARENTHESIS:		(
+// Token CONST_STRING:			", "
+// Token RIGHT_PARENTHESIS:		)
+// Token SEMICOLON:			;
+// Token KEYWORD_ENDIF:			endif
+// Token SEMICOLON:			;
+// Token KEYWORD_ENDFOR:			endfor
+// Token SEMICOLON:			;
+// Token IDENTIFIER:			writeStr
+// Token LEFT_PARENTHESIS:		(
+// Token CONST_STRING:			"\n"
+// Token RIGHT_PARENTHESIS:		)
+// Token SEMICOLON:			;
+// Token KEYWORD_ENDDEF:			enddef
+// Token SEMICOLON:			;
+// Token KEYWORD_DEF:			def
+// Token KEYWORD_MAIN:			main
+// Token LEFT_PARENTHESIS:		(
+// Token RIGHT_PARENTHESIS:		)
+// Token COLON:			:
+// Token KEYWORD_CONST:			const
+// Token IDENTIFIER:			arrSize
+// Token ASSIGN_OP:			=
+// Token CONST_INT:			5
+// Token COLON:			:
+// Token KEYWORD_INT:			integer
+// Token SEMICOLON:			;
+// Token IDENTIFIER:			arr
+// Token LEFT_BRACKET:		[
+// Token IDENTIFIER:			arrSize
+// Token RIGHT_BRACKET:		]
+// Token COLON:			:
+// Token KEYWORD_INT:			integer
+// Token SEMICOLON:			;
+// Token IDENTIFIER:			arr
+// Token LEFT_BRACKET:		[
+// Token CONST_INT:			0
+// Token RIGHT_BRACKET:		]
+// Token ASSIGN_OP:			=
+// Token CONST_INT:			2
+// Token SEMICOLON:			;
+// Token IDENTIFIER:			arr
+// Token LEFT_BRACKET:		[
+// Token CONST_INT:			1
+// Token RIGHT_BRACKET:		]
+// Token ASSIGN_OP:			=
+// Token CONST_INT:			3
+// Token SEMICOLON:			;
+// Token IDENTIFIER:			arr
+// Token LEFT_BRACKET:		[
+// Token CONST_INT:			2
+// Token RIGHT_BRACKET:		]
+// Token ASSIGN_OP:			=
+// Token CONST_INT:			4
+// Token SEMICOLON:			;
+// Token IDENTIFIER:			arr
+// Token LEFT_BRACKET:		[
+// Token CONST_INT:			3
+// Token RIGHT_BRACKET:		]
+// Token ASSIGN_OP:			=
+// Token CONST_INT:			10
+// Token SEMICOLON:			;
+// Token IDENTIFIER:			arr
+// Token LEFT_BRACKET:		[
+// Token CONST_INT:			4
+// Token RIGHT_BRACKET:		]
+// Token ASSIGN_OP:			=
+// Token CONST_INT:			40
+// Token SEMICOLON:			;
+// Token IDENTIFIER:			writeStr
+// Token LEFT_PARENTHESIS:		(
+// Token CONST_STRING:			"Array: "
+// Token RIGHT_PARENTHESIS:		)
+// Token SEMICOLON:			;
+// Token IDENTIFIER:			printArray
+// Token LEFT_PARENTHESIS:		(
+// Token IDENTIFIER:			arr
+// Token COMMA:			,
+// Token IDENTIFIER:			arrSize
+// Token RIGHT_PARENTHESIS:		)
+// Token SEMICOLON:			;
+// Token IDENTIFIER:			writeStr
+// Token LEFT_PARENTHESIS:		(
+// Token CONST_STRING:			"Enter element to search: "
+// Token RIGHT_PARENTHESIS:		)
+// Token SEMICOLON:			;
+// Token IDENTIFIER:			x
+// Token COLON:			:
+// Token KEYWORD_INT:			integer
+// Token SEMICOLON:			;
+// Token IDENTIFIER:			x
+// Token ASSIGN_OP:			=
+// Token FN_readInteger:			readInteger
+// Token LEFT_PARENTHESIS:		(
+// Token RIGHT_PARENTHESIS:		)
+// Token SEMICOLON:			;
+// Token IDENTIFIER:			result
+// Token COLON:			:
+// Token KEYWORD_INT:			integer
+// Token SEMICOLON:			;
+// Token IDENTIFIER:			result
+// Token ASSIGN_OP:			=
+// Token IDENTIFIER:			binarySearch
+// Token LEFT_PARENTHESIS:		(
+// Token IDENTIFIER:			arr
+// Token COMMA:			,
+// Token IDENTIFIER:			arrSize
+// Token COMMA:			,
+// Token IDENTIFIER:			x
+// Token RIGHT_PARENTHESIS:		)
+// Token SEMICOLON:			;
+// Token KEYWORD_IF:			if
+// Token LEFT_PARENTHESIS:		(
+// Token IDENTIFIER:			result
+// Token NOT_EQUAL_OP:			!=
+// Token MINUS_OP:			-
+// Token CONST_INT:			1
+// Token RIGHT_PARENTHESIS:		)
+// Token COLON:			:
+// Token IDENTIFIER:			writeStr
+// Token LEFT_PARENTHESIS:		(
+// Token CONST_STRING:			"Element found at index "
+// Token RIGHT_PARENTHESIS:		)
+// Token SEMICOLON:			;
+// Token FN_writeInteger:			writeInteger
+// Token LEFT_PARENTHESIS:		(
+// Token IDENTIFIER:			result
+// Token RIGHT_PARENTHESIS:		)
+// Token SEMICOLON:			;
+// Token IDENTIFIER:			writeStr
+// Token LEFT_PARENTHESIS:		(
+// Token CONST_STRING:			"\n"
+// Token RIGHT_PARENTHESIS:		)
+// Token SEMICOLON:			;
+// Token KEYWORD_ELSE:			else
+// Token COLON:			:
+// Token IDENTIFIER:			writeStr
+// Token LEFT_PARENTHESIS:		(
+// Token CONST_STRING:			"Element not found in array\n"
+// Token RIGHT_PARENTHESIS:		)
+// Token SEMICOLON:			;
+// Token KEYWORD_ENDIF:			endif
+// Token SEMICOLON:			;
+// Token KEYWORD_ENDDEF:			enddef
+// Token SEMICOLON:			;
+/* program */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "lambdalib.h"
+
+int binarySearch(int *arr, int size, int x)
+{
+    int l, r, mid;
+    l = 0;
+    r = size - 1;
+    while (l <= r)
+    {
+        mid = l + (r - l) / 2;
+        if (arr[mid] == x)
+        {
+            return mid;
+        }
+
+        if (arr[mid] < x)
+        {
+            l = mid + 1;
+        }
+        else
+        {
+            r = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
+void printArray(int *arr, int size)
+{
+    for (int i = 0; i <= size - 1; i++)
+    {
+        writeInteger(arr[i]);
+        if (i != size - 1)
+        {
+            writeStr(", ");
+        }
+    }
+    writeStr("\n");
+}
+
+int main()
+{
+    const int arrSize = 5;
+    int arr[arrSize];
+    arr[0] = 2;
+    arr[1] = 3;
+    arr[2] = 4;
+    arr[3] = 10;
+    arr[4] = 40;
+    writeStr("Array: ");
+    printArray(arr, arrSize);
+    writeStr("Enter element to search: ");
+    int x;
+    x = readInteger();
+    int result;
+    result = binarySearch(arr, arrSize, x);
+    if (result != -1)
+    {
+        writeStr("Element found at index ");
+        writeInteger(result);
+        writeStr("\n");
+    }
+    else
+    {
+        writeStr("Element not found in array\n");
+    }
+}
+
+/* Your program is syntactically correct! */
